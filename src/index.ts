6f8da180b5d89341efb46fa04e3d0a06c4c432ed
David Chanin <chanindav@gmail.com>
2019-04-22 15:08:36 +0100

    fix: exporting base Rewriter class

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as RewriteHandler } from './RewriteHandler';
+export { default as Rewriter } from './rewriters/Rewriter';
 export { default as FieldArgNameRewriter } from './rewriters/FieldArgNameRewriter';
 export { default as FieldArgsToInputTypeRewriter } from './rewriters/FieldArgsToInputTypeRewriter';
 export { default as FieldArgTypeRewriter } from './rewriters/FieldArgTypeRewriter';
