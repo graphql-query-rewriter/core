f67ca802fdaf5f116453055d928ff983940ef81c
Andrés Pérez Manríquez <apandresipm@gmail.com>
2022-05-26 12:52:05 -0400

    forgot to export new rewriter

--- src/rewriters/index.ts
+++ src/rewriters/index.ts
@@ -5,3 +5,4 @@ export { default as FieldArgTypeRewriter } from './FieldArgTypeRewriter';
 export { default as NestFieldOutputsRewriter } from './NestFieldOutputsRewriter';
 export { default as ScalarFieldToObjectFieldRewriter } from './ScalarFieldToObjectFieldRewriter';
 export { default as JsonToTypedObjectRewriter } from './JsonToTypedObjectRewriter';
+export { default as FieldRewriter } from './FieldRewriter';
